057f5ef9adf6d1e0d2f9c8366d0dc65607d0b172
Okoromi Victor <okoromivictorsunday@gmail.com>
2022-01-21 10:54:43 +0100

    Reservation name changed

--- src/export.js
+++ src/export.js
@@ -4,7 +4,7 @@ export const Home = lazy(() => import("./Pages/Home/Home"));
 export const SearchResult = lazy(() => import('./Pages/SearchResult/SearchResult'))
 export const PropertyDetails = lazy(() => import('./Pages/PropertyDetails/PropertyDetails'))
 export const Payments = lazy(() => import('./Pages/payments/Payments') )
-export const Reservation = lazy(() => import('./Pages/payments/Reservation.jsx') )
+export const Reservation = lazy(() => import('./Pages/payments/reservation.jsx') )
 
 export { default as Footer } from "./components/Footer/Footer"
 export { default as NotFound } from "./components/NotFound"
